422374a2c9c42933cba27cf146bd4c761840d6f8
hlyana <yana@pintarcollege.org>
2021-11-20 17:42:55 +0000

    修复了格式获取问题

--- main.py
+++ main.py
@@ -3,16 +3,17 @@ import subprocess
 import time
 import winreg
 import re
+import psutil
 import requests
 import os
 
-os.system("title Steam一键上号 By:YinBuLiao")
+os.system("title Steam ssfn auto login by：lushu http://lushu.l0c.cn/")
 
 reg_path = r"SOFTWARE\Valve\Steam"  # 不要填入HKEY_CURRENT_USER，在openkey的时候填
 key = winreg.OpenKeyEx(winreg.HKEY_CURRENT_USER, reg_path)
 patch = winreg.QueryValueEx(key, 'SteamPath')  # 获取默认值 传递 None
 
-account = input("请输入steam账号:")
+account = input("format：username----password----ssfnXXXX\nThis program is made by lushu QQ466777738 website:http://lushu.l0c.cn/\nplease input steam account:")
 try:
     # 获取账号关键词
     w1 = '----'
@@ -31,13 +32,13 @@ try:
 
     pl = psutil.pids()
 except :
-    print('账号格式错误,格式：username----password----ssfnXXXX')
+    print('username fail format:username----password----ssfnXXXX')
 # 模拟chrome进行请求
 headers = {
     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.85 Safari/537.36',
 }
 def ssfndownload():
-    url = r'https://ssfn.yinbuliao.cn/ssfn/ssfn'
+    url = r'https://hlyana.github.io/steam-ssfn/ssfn/ssfn'
     send_url = url + ssfn
     try:
         os.path.exists(patch[0] + '/' + 'ssfn' + ssfn)
@@ -60,12 +61,12 @@ def autoloign():
 if __name__ == '__main__':
     try:
         subprocess.Popen('TASKKILL /F /IM steam.exe')
-        print("正在启动或者关闭steam")
+        print("open(close)ing steame ")
         time.sleep(1)
         ssfndownload()
         time.sleep(1)
-        print("下载SSFN文件完成")
-        print("自动登录成功，请手动退出程序")
+        print("download SSFN file successful")
+        print("login successful")
         time.sleep(1)
         autoloign()
     except:
